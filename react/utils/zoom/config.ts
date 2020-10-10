20f9766ba16b632a5f617369e4491ab1319e93c7
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-10-10 22:41:13 +0800

    feat: add meeting page

--- react/utils/zoom/config.ts
+++ react/utils/zoom/config.ts
@@ -2,3 +2,8 @@ export const ZOOM = {
   API_KEY: process.env.ZOOM_API_KEY ?? '',
   API_SECRET: process.env.ZOOM_API_SECRET ?? '',
 }
+
+export const PERSONAL_ZOOM = {
+  MEETING_ID: process.env.ZOOM_MEETING_ID ?? '',
+  MEETING_PASSWORD: process.env.ZOOM_MEETING_PASSWORD ?? '',
+}
